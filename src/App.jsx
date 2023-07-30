b2e1a16abfc1f44e19e71902be4f7c11e31dbd9f
osman goni <osman.ice.32@gmail.com>
2023-07-30 23:25:54 +0600

    homepage

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Navbar from "./component/Navbar";
+import TodoList from "./component/Todolist";
+function App() {
+  return (
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <Navbar />
+
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+        <Header />
+
+        <hr className="mt-4" />
+
+        <TodoList />
+
+        <hr className="mt-4" />
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
